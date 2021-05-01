17855d10d932c9e871644fc427df71d9c2ef1651
M-Riku <luminghao1994@yahoo.co.jp>
2021-05-01 21:02:50 +0900

    添加显示所有玩家的的剩余收牌

--- client/assets/Script/GameCtrl.ts
+++ client/assets/Script/GameCtrl.ts
@@ -15,28 +15,38 @@ export default class GameCtrl {
 
     private setHolePokersBtn: cc.Button = null;
 
-    public curPlayer: string = null;
+    private curPlayerLable: cc.Label = null;
 
-    public playPokers: { pokers: Poker[] } = { pokers: [] };
+    private otherPlayerLable: cc.Label[] = null;
 
-    public Init(pockerContainer: cc.Node, pokerPrefab: cc.Prefab, setHolePokersBtn: cc.Button,
-        playPokersBtn: cc.Button, regretPokersBtn: cc.Button, curPlayer: string) {
+    private curPlayer: string = null;
+
+    private otherPlayers: string[] = [];
+
+    private playPokers: { pokers: Poker[] } = { pokers: [] };
+
+    public Init(pockerContainer: cc.Node, pokerPrefab: cc.Prefab,
+        playPokersBtn: cc.Button, regretPokersBtn: cc.Button, setHolePokersBtn: cc.Button,
+        curPlayerLable: cc.Label, otherPlayerLable: cc.Label[], curPlayer: string) {
         this.pokerContainer = pockerContainer;
         this.pokerPrefab = pokerPrefab;
         this.playPokersBtn = playPokersBtn;
         this.regretPokersBtn = regretPokersBtn;
         this.setHolePokersBtn = setHolePokersBtn;
+        this.curPlayerLable = curPlayerLable;
+        this.otherPlayerLable = otherPlayerLable;
+        this.curPlayer = curPlayer;
         this.playPokersBtn.node.on('click', this.OnPlayPokersBtnClick.bind(this));
         this.regretPokersBtn.node.on('click', this.OnRegretPokersBtnClick.bind(this));
         this.setHolePokersBtn.node.on('click', this.OnSetHolePokersBtnClick.bind(this));
-        this.curPlayer = curPlayer;
+        this.getOtherPlayersName();
     }
 
     private OnPlayPokersBtnClick() {
         console.log('play pokers');
         let xhr = new XMLHttpRequest();
         let data = JSON.stringify(this.playPokers.pokers);
-        xhr.open("POST", `/game/play-cards/${this.curPlayer}`, true);
+        xhr.open("POST", `http://localhost:3000/game/play-cards/${this.curPlayer}`, true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(data);
         this.playPokers.pokers = [];
@@ -45,7 +55,7 @@ export default class GameCtrl {
     private OnRegretPokersBtnClick() {
         console.log('regret pokers');
         let xhr = new XMLHttpRequest();
-        xhr.open("POST", `/game/regret-cards/${this.curPlayer}`, true);
+        xhr.open("POST", `http://localhost:3000/game/regret-cards/${this.curPlayer}`, true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send();
     }
@@ -54,10 +64,19 @@ export default class GameCtrl {
         console.log('set hole pokers');
         let xhr = new XMLHttpRequest();
         let data = JSON.stringify(this.playPokers.pokers);
-        xhr.open("POST", `/game/set-hole-pokers/${this.curPlayer}`, true);
+        xhr.open("POST", `http://localhost:3000/game/set-hole-pokers/${this.curPlayer}`, true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(data);
         this.playPokers.pokers = [];
+    }
+
+    private getOtherPlayersName() {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", `http://localhost:3000/game/list-other-players/${this.curPlayer}`, false);
+        xhr.onload = () => {
+            this.otherPlayers = JSON.parse(xhr.responseText);
+        };
+        xhr.send();
     }
 
     private CreateUIPoker(poker: Poker, x: number, y: number): UIPoker {
@@ -68,7 +87,7 @@ export default class GameCtrl {
         return uiPoker
     }
 
-    public ShowUIPoker(curPokers: any, myPlayedPokers: any, otherPlayerPokers: any, otherPlayer: any) {
+    public ShowUIPoker(curPokers: any, myPlayedPokers: any, otherPlayerPokers: any) {
         if (curPokers.length > 31) {
             this.setHolePokersBtn.node.active = true;
         } else {
@@ -77,6 +96,10 @@ export default class GameCtrl {
 
         this.playPokers.pokers = [];
 
+        // 描绘当前玩家名称和手牌数量
+        this.curPlayerLable.string = `${this.curPlayer}: ${curPokers.length}张`;
+
+        // 描绘当前玩家的手牌
         this.pokerContainer.destroyAllChildren();
         this.SortPokers(curPokers);
         let startX: number = -Math.floor(curPokers.length / 2) * 30;
@@ -86,6 +109,7 @@ export default class GameCtrl {
             this.pokerContainer.addChild(uiPoker.node);
         });
 
+        // 描绘当前玩家所出的牌
         this.SortPokers(myPlayedPokers);
         startX = -Math.floor(myPlayedPokers.length / 2) * 30;
         startY = -100;
@@ -95,13 +119,17 @@ export default class GameCtrl {
             this.pokerContainer.addChild(uiPoker.node);
         })
 
-        otherPlayer.forEach((player, index) => {
-            let otherPlayedPoker = otherPlayerPokers.find(
+        // 描绘其他玩家
+        this.otherPlayers.forEach((player, index) => {
+            let otherPlayer = otherPlayerPokers.find(
                 playerPoker => playerPoker.playerId === player
-            ).playedPokers
-            startX = PlayerPostion[index].x - Math.floor(otherPlayedPoker.length / 2) * 30;;
+            );
+            // 描绘其他玩家的名称和剩余牌数
+            this.otherPlayerLable[index].string = `${player}: ${otherPlayer.pokers.length}张`;
+            startX = PlayerPostion[index].x - Math.floor(otherPlayer.playedPokers.length / 2) * 30;;
             startY = PlayerPostion[index].y;
-            otherPlayedPoker.forEach((poker, indexP) => {
+            // 描绘其他玩家当前出的牌
+            otherPlayer.playedPokers.forEach((poker, indexP) => {
                 let uiPoker = this.CreateUIPoker(poker, startX + 30 * indexP, startY);
                 uiPoker.selectBtn.node.active = false;
                 this.pokerContainer.addChild(uiPoker.node);

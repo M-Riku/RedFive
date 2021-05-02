bfa1634856ad8f3306ff9370df0442e78642035d
M-Riku <luminghao1994@yahoo.co.jp>
2021-05-02 11:54:59 +0900

    重构代码

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+
 const cors = require('cors');
-const app = express();
-app.use(cors());
-app.use(express.json());
-const WebSocketServer = require("ws").Server;
+
+const ws = require('ws');
 
 const gameAdminRoutes = require('./routes/gameAdmin');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 const server = app.listen(3000);
-const wss = new WebSocketServer({ server: server, path: "/wsgame" });
+
+const wss = new ws.Server({ server: server, path: "/wsgame" });
 
 sendFlag = [];
 players = require('./models/Players');
